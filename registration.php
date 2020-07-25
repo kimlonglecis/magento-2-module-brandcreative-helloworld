393ebc1d234a74f27cec0bdcd69ccaac2e94dc36
long le <kimlonglecis@gmail.com>
2020-07-25 15:18:56 -0700

    minor changes

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Brandcreative_Blank',
+    'Brandcreative_HelloWorld',
     __DIR__
 );
